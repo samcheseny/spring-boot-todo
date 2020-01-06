99f87b3ce9e57c7db27d3494633ce9aaf3e4dcf1
Samuel <ndarasam@gmail.com>
2020-01-06 18:24:54 +0300

    Add checkstyle

--- src/main/java/com/samcheseny/springboottodo/SpringBootTodoApplication.java
+++ src/main/java/com/samcheseny/springboottodo/SpringBootTodoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootTodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTodoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootTodoApplication.class, args);
+  }
 
 }
